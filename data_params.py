549de20c39c6543d9ba879d7a3cad441f19da78e
Subha0009 <subha0009@gmail.com>
2021-07-01 19:31:13 +0530

    update params

--- data_params.py
+++ data_params.py
@@ -5,5 +5,5 @@ en_fr_params = {"path_to_mt_vocab":"Data/vocab.enfr.32000",
                 "path_to_src":"Data/test_en-fr_en.pkl",
                 "path_to_tar":"Data/test_en-fr_fr.pkl",}
 de_en_params = {"path_to_mt_vocab":"Data/vocab.de-en.32000",
-                "path_to_src":"Data/test_en-fr_en.pkl",
-                "path_to_tar":"Data/test_en-fr_fr.pkl",}
+                "path_to_src":"Data/iwslt_test_de",
+                "path_to_tar":"Data/iwslt_test_en",}
